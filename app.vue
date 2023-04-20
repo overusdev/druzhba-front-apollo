c16ddbe9b64378cd0790798c1a7d24b6198c154c
Сергей Вилков <sergey.vilkov@rsv.ru>
2023-04-20 16:44:16 +0300

    Init

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <pre>{{ data?.data?.pets.length }}</pre>
+    <button @click="onClick">click</button>
+  </div>
+</template>
+
+<!-- <script lang="ts" setup> -->
+<script setup>
+const query = gql`
+  query findAll {
+    pets {
+      id
+      name
+    }
+  }
+`
+let data = ref(null);
+
+async function onClick (){
+  return ( data.value = await useAsyncQuery(query) );
+}
+</script>
