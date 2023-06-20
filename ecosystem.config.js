fcb017cf51944b80f9a68f0837bfb50307588c83
root <root@vm1120560.stark-industries.solutions>
2023-06-20 09:58:23 +0300

    Ecosystem config edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,18 @@
+const os = require('os');
 module.exports = {
     apps: [
       {
         name: 'DruzbaFront',
         port: '3000',
-        exec_mode: 'cluster',
+        //exec_mode: 'cluster',
         instances: 'max',
-        script: './.output/server/index.mjs'
+        watch: true,
+        script: './.output/server/index.mjs',
+        instances   : os.cpus().length,
+        exec_mode   : 'fork',
+        env: {
+            NODE_ENV: "production",
+        }
       }
     ]
 }
