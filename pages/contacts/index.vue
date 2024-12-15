7b400bd45a87a496001ae3b71d07993a1c9e7c47
Сергей Вилков <sergey.vilkov@rsv.ru>
2024-12-15 12:07:54 +0300

    Correct contacts

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -2,10 +2,10 @@
   <div class="contacts">
     <div class="container">
       <PageTitle title="Контакты" class="contacts__page-title"/>
-        <p class="news-item__note-description" v-html="contactsData.theme"
-        ></p>
+        <!-- <p class="news-item__note-description" v-html="contactsData.theme"
+        ></p> -->
       <client-only>
-        <!-- <p class="contacts__title">Схема СНТ Дружба</p>
+        <p class="contacts__title">Схема СНТ Дружба</p>
         <img src="~/assets/images/contacts/contacts_sсheme.png"
           alt="Схема СНТ Дружба"
           class="contacts__scheme"
@@ -14,7 +14,7 @@
         <img src="~/assets/images/contacts/ortophotoplan.png"
           alt="Оотофотоплан СНТ Дружба"
           class="contacts__scheme"
-        > -->
+        >
         <!-- <YandexMap
           :coordinates="coordinates"
           :detailed-controls="detailedControls"
@@ -22,7 +22,7 @@
           map-type="map"
           zoom="15"
           type="Point"
-        /> -->
+        />-->
       </client-only>
     </div>
   </div>
@@ -41,40 +41,40 @@ export default {
     const coordinates = [56.645999, 44.168635];
     const controls = ['fullscreenControl'];
     const detailedControls = { zoomControl: { position: { right: 10, top: 50 } } };
-    const contactsData = reactive({
-        theme: '',
-    });
-    const CONTACTS = gql`
-        query findOne($id: Int!) {
-            contact(id: $id) {
-                id
-                theme
-            }
-        }
-    `;
-    const { result, loading, error, refetch } = useQuery(CONTACTS, () => ({
-        id: 1,
-    }));
-    const contacts = computed(() => result ?? {});
+    // const contactsData = reactive({
+    //     theme: '',
+    // });
+    // const CONTACTS = gql`
+    //     query findOne($id: Int!) {
+    //         contact(id: $id) {
+    //             id
+    //             theme
+    //         }
+    //     }
+    // `;
+    // const { result, loading, error, refetch } = useQuery(CONTACTS, () => ({
+    //     id: 1,
+    // }));
+    // const contacts = computed(() => result ?? {});
 
-    onMounted(async () => {
-        const refetchQuery = await refetch();
-        if(refetchQuery.data.contact) {
-            contactsData.theme = refetchQuery.data.contact.theme;
-        }
-    });
+    // onMounted(async () => {
+    //     const refetchQuery = await refetch();
+    //     if(refetchQuery.data.contact) {
+    //         contactsData.theme = refetchQuery.data.contact.theme;
+    //     }
+    // });
     return {
       store,
       PageTitle,
-      coordinates,
-      controls,
-      detailedControls,
+      // coordinates,
+      // controls,
+      // detailedControls,
       
-      contactsData,
-      result,
-      loading,
-      refetch,
-      contacts,
+      // contactsData,
+      // result,
+      // loading,
+      // refetch,
+      // contacts,
     }
   }
 }
