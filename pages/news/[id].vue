de1afb7f14b06f2079ad2ab3c57526022a4e7d92
Сергей Вилков <sergey.vilkov@rsv.ru>
2024-12-15 12:05:43 +0300

    Correct news

--- pages/news/[id].vue
+++ pages/news/[id].vue
@@ -1,54 +1,61 @@
 <template>
     <div class="news-item">
       <div class="container">
-        <PageTitle :title="newsData.name"/>
-        <p class="news-item__note-description" v-html="newsData.theme"
+        <PageTitle :title="news.title"/>
+        <p class="news-item__note-description" v-html="news.noteDescription"
         ></p>
+        <p class="news-item__note-description" v-html="news.description"></p>
+        <!-- <PageTitle :title="newsData.name"/>
+        <p class="news-item__note-description" v-html="newsData.theme"
+        ></p> -->
       </div>
     </div>
 </template>
 
 <script lang="js">
+import { useNews } from "~/stores/news";
 import PageTitle from '~/components/page-title/PageTitle.vue';
 
 export default {
   setup () {
+    const store = useNews();
     const router = useRouter();
     const route = useRoute();
-    const newsData = reactive({
-        name: '',
-        theme: '',
-    });
-    const NEWS = gql`
-        query findOne($id: Int!) {
-            new(id: $id) {
-              id
-              name
-              theme
-            }
-        }
-    `;
-    const { result, loading, error, refetch } = useQuery(NEWS, () => ({
-        id: Number(route.params.id),
-    }));
-    const news = computed(() => result ?? {});
+    const news = store.getNewsById({ id: route.params.id });
+    // const newsData = reactive({
+    //     name: '',
+    //     theme: '',
+    // });
+    // const NEWS = gql`
+    //     query findOne($id: Int!) {
+    //         new(id: $id) {
+    //           id
+    //           name
+    //           theme
+    //         }
+    //     }
+    // `;
+    // const { result, loading, error, refetch } = useQuery(NEWS, () => ({
+    //     id: Number(route.params.id),
+    // }));
+    // const news = computed(() => result ?? {});
 
-    onMounted(async () => {
-        const refetchQuery = await refetch();
-        if(refetchQuery.data.new) {
-            newsData.id = refetchQuery.data.new.id;
-            newsData.name = refetchQuery.data.new.name;
-            newsData.theme = refetchQuery.data.new.theme;
-        }
-    });
+    // onMounted(async () => {
+    //     const refetchQuery = await refetch();
+    //     if(refetchQuery.data.new) {
+    //         newsData.id = refetchQuery.data.new.id;
+    //         newsData.name = refetchQuery.data.new.name;
+    //         newsData.theme = refetchQuery.data.new.theme;
+    //     }
+    // });
 
         return {
-            newsData,
-            result,
-            loading,
-            refetch,
-            router,
-            route,
+            // newsData,
+            // result,
+            // loading,
+            // refetch,
+            // router,
+            // route,
             news,
             PageTitle,
         }
